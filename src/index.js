1a350662a46631ed57be54a9a7c6ee04f5cdcc5e
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-04-22 19:30:30 +0530

    Utils

--- src/index.js
+++ src/index.js
@@ -26,3 +26,16 @@ try {
 }
 })()*/
 connectDB()
+.then(()=>{
+    app.on("errror",(error)=>{
+        console.log("ERRR: ",error);
+        throw error
+    })
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed!!!!",err)
+})
