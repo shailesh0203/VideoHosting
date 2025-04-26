299ea6273d90fe25f968a08d7c235467ed9dc472
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-04-26 11:45:27 +0530

    tokens,bcrypt,aggregate

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema=Schema({
     videoFile:{
         type:String,//url cloudinary
@@ -34,4 +35,6 @@ const videoSchema=Schema({
     }
 
 },{timestamps:true})
+videoSchema.plugin(mongooseAggregatePaginate)
+
 export const Video=mongoose.model("Video",videoSchema)
