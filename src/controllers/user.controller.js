d59f7b388c4c3528542d76548b6ced728f282b7a
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-05-07 00:12:13 +0530

    loginUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,22 @@ import {ApiError} from "../utils/ApiError.js"
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import {ApiResponse} from "../utils/ApiResponse.js"
+const generateAccessAndRefereshTokens = async(userId) =>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
 const registerUser=AsyncHandler(async(req,res)=>{
 //get user details from frontend
 //validation-not empty
@@ -27,12 +43,15 @@ if(existedUser){
 }
 
 const avatarLocalPath=req.files?.avatar[0]?.path;
-const coverImageLocalPath=req.files?.coverImage[0]?.path;
+const coverImageLocalPath=req.files?.coverImage?.[0]?.path;
 if(!avatarLocalPath){
     throw new ApiError(400,"Avatar file is required")
 }
 const avatar=await uploadOnCloudinary(avatarLocalPath)
-const coverImage=await uploadOnCloudinary(coverImageLocalPath)
+let coverImage;
+if(coverImageLocalPath){
+coverImage=await uploadOnCloudinary(coverImageLocalPath)
+}
 if(!avatar){
     throw new ApiError(400,"avatar file is required")
 }
@@ -57,6 +76,27 @@ return res.status(201).json(
     new ApiResponse(200, createdUser, "User registered Successfully")
 )
 })
+const loginUser=AsyncHandler(async(req,res)=>{
+//req body->data
+//username or email
+//find the user
+//check password
+//generate access and refresh token and send it to the user
+//send cookies
+
+const {username,email,password}=req.body
+if(!username && !email){
+    throw new ApiError(404,"email or username is required");
+}
+const user=await User.findOne({
+    $or:[{username},{email}]
+})
+const isPasswordValid=await user.isPasswordCorrect(password);
+if(!isPasswordValid){
+    throw new ApiError(401,"Invalid user credentials")
+}
+const {accessToken,refreshToken}=await generateAccessAndRefereshTokens(user._id);
+})
 
 
-export {registerUser}
+export {registerUser,loginUser}
