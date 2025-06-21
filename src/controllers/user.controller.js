87ce4053d82d3c3974e7aabb893b1e52486c345a
Shailesh Kumar <shaileshyadav02032003@gmail.com>
2025-06-21 23:54:44 +0530

    watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -338,5 +338,64 @@ const getUserChannelProfile=AsyncHandler(async(req,res)=>{
             }
         }
     ])
+    if(!channel?.length){
+        throw new ApiError(404,"channel does not exist")
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,channel[0],"User Channel fetched successsully")
+    )
 })
-export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile}
+const getWatchHistory=AsyncHandler(async(req,res)=>{
+    const user= await User.aggregate([
+      {
+        $match:{
+            _id:new mongoose.Types.ObjectId(req.user._id)
+        }
+      },
+      {
+        $lookup:{
+            from:"videos",
+            localField:"watchHistory",
+            foreignField:"_id",
+            as:"watchHistory",
+            pipeline:[
+                {
+                     $lookup:{
+                        from:"users",
+                        localField:"owner",
+                        foreignField:"_id",
+                        as:"owner",
+                        pipeline:[
+                            {
+                                $project:{
+                                    fullName:1,
+                                    userName:1,
+                                    avatar:1
+                                }
+                            }
+                        ]
+                     }
+                },
+                {
+                    $addFields:{
+                        owner:{
+                            $first:"$owner"
+                        }
+                    }
+                }
+            ]
+        }
+      }
+    ])
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
+})
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory}
