d58a4547250ef9730a7fc8978748e23c41759f7a
Shailesh Kumare <shaileshyadav02032003@gmail.com>
2025-07-25 22:20:27 +0530

    dashboard

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,6 +8,45 @@ import {AsyncHandler} from "../utils/AsyncHandler.js"
 
 const getChannelStats = AsyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const allLikes=await Like.aggregate([
+        {
+            $match:{
+                likedBy:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $group:{
+                _id:null,
+                totalVideoLikes:{
+                    $sum:{
+                        $cond:[
+                            {$ifNull:["$video",false]},
+                            1,//add 1
+                            0
+                        ]
+                    }
+                },
+                totalTweetLikes:{
+                    $sum:{
+                        $cond:[
+                            {$ifNull:["$tweet",false]},
+                            1,
+                            0
+                        ]
+                    }
+                },
+                totalCommentLikes:{
+                    $sum:{
+                        $cond:[
+                            {$ifNull:["$comment",false]},
+                            1,
+                            0
+                        ]
+                    }
+                }
+            }
+        }
+    ])
 })
 
 const getChannelVideos = AsyncHandler(async (req, res) => {
