b4dc7a8824ea73f18c15b34904d374076865a689
Shailesh Kumare <shaileshyadav02032003@gmail.com>
2025-07-26 01:12:46 +0530

    dash

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,6 +8,7 @@ import {AsyncHandler} from "../utils/AsyncHandler.js"
 
 const getChannelStats = AsyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  //total likes
     const allLikes=await Like.aggregate([
         {
             $match:{
@@ -47,10 +48,83 @@ const getChannelStats = AsyncHandler(async (req, res) => {
             }
         }
     ])
+    //total subscribers
+    const allSubscribers=await Subscription.aggregate([
+        {
+            $match:{
+                channel:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $count:"subscribers"
+        }
+    ])
+    //total videos
+    const allVideo=await Video.aggregate([
+        {
+            $match:{
+                videoOwner: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $count:"Videos"
+        }
+    ])
+    //total views
+    const allViews=await Video.aggregate([
+        {
+            $match:{
+                videoOwner:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $group:{
+                _id:null,
+                allVideoViews:{
+                    $sum:"$views"
+                }
+            }
+        }
+    ])
+    const stats={
+        Subscribers:allSubscribers[0].subscribers,
+        totalVideos:allVideo[0].Videos,
+        totalVideoViews:allViews[0].allVideoViews,
+        totalVideoLikes:allLikes[0].totalVideoLikes,
+        totalTweetLikes:allLikes[0].totalTweetLikes,
+        totalCommentLikes:allLikes[0].totalCommentLikes
+    }
+
+    //return response
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            stats,
+            "fetching channel stats succesully"
+        )
+    )
+
 })
 
 const getChannelVideos = AsyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const allVideo=await Video.find(
+        {
+            videoOwner:req.user._id
+        }
+    )
+    if(!allVideo){
+        throw new ApiError(500,
+            "something went wrong while fetching channel all videos"
+        )
+    }
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            allVideo,
+            "All videos fetched successfully!!"
+        )
+    )
 })
 
 export {
