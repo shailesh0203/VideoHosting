cbb075f668b2145dc0ec613bfaf149cbabf13794
Shailesh Kumare <shaileshyadav02032003@gmail.com>
2025-07-26 22:21:39 +0530

    healthcheck and like controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -5,7 +5,29 @@ import {AsyncHandler} from "../utils/AsyncHandler.js"
 
 const healthcheck = AsyncHandler(async (req, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
-})
+    let healthCheck={
+        uptime:process.uptime(),
+        message:'ok',
+        responsetime:process.hrtime(),
+        timestamp:Date.now()
+    }
+    try {
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            healthCheck,
+            "health is good"
+        )
+    )
+} catch (error) {
+    console.error("error in health check",error)
+    healthCheck.message=error;
+    throw new ApiError(503,
+        "getting error in healthcheck Time"
+    )
+}
+
+});
 
 export {
     healthcheck
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,27 +3,196 @@ import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {AsyncHandler} from "../utils/AsyncHandler.js"
+import { User } from "../models/user.model.js"
 
 const toggleVideoLike = AsyncHandler(async (req, res) => {
     const {videoId} = req.params
-    //TODO: toggle like on video
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400,"This video is not valid")
+    }
+//find video already liked or not
+const videoLike=await Like.findOne(
+    {
+        video:videoId,
+        likedBy:req.user._id
+    }
+)
+let like;
+let unlike;
+if(videoLike){
+    unlike=await Like.deleteOne({
+        video:videoId
+    })
+    if(!unlike){
+        throw new ApiError(500,
+            "something went wrong while unliking the video"
+        )
+    }
+}else{
+    like=await Like.create({
+        video:videoId,
+        likedBy:req.user._id
+    })
+    if(!like){
+        throw new ApiError(
+            500,
+            "something went wrong while liking the video"
+        )
+    }
+}
+return res.status(200).json(
+    new ApiResponse(200,{},`user ${like?"like":"unlike"} video successsfully`)
+)
 })
 
 const toggleCommentLike = AsyncHandler(async (req, res) => {
     const {commentId} = req.params
-    //TODO: toggle like on comment
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400,"This comment is not valid")
+    }
+//find video already liked or not
+const commentLike=await Like.findOne(
+    {
+        comment:commentId,
+        likedBy:req.user._id
+    }
+)
+let like;
+let unlike;
+if(commentLike){
+    unlike=await Like.deleteOne({
+        comment:commentId
+    })
+    if(!unlike){
+        throw new ApiError(500,
+            "something went wrong while unliking the comment"
+        )
+    }
+}else{
+    like=await Like.create({
+       comment:commentId,
+        likedBy:req.user._id
+    })
+    if(!like){
+        throw new ApiError(
+            500,
+            "something went wrong while liking the comment"
+        )
+    }
+}
+return res.status(201).json(
+    new ApiResponse(200,{},`user ${like?"like":"unlike"} comment successsfully`)
+)
 
 })
 
 const toggleTweetLike = AsyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
+     const {tweetId} = req.params
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400,"This tweet is not valid")
+    }
+//find video already liked or not
+const tweetLike=await Like.findOne(
+    {
+       tweet:tweetId,
+       likedBy:req.user._id
+    }
+)
+let like;
+let unlike;
+if(tweetLike){
+    unlike=await Like.deleteOne({
+        tweet:tweetId
+    })
+    if(!unlike){
+        throw new ApiError(500,
+            "something went wrong while unliking the tweet"
+        )
+    }
+}else{
+    like=await Like.create({
+       tweet:tweetId,
+        likedBy:req.user._id
+    })
+    if(!like){
+        throw new ApiError(
+            500,
+            "something went wrong while liking the tweet"
+        )
+    }
+}
+return res.status(201).json(
+    new ApiResponse(200,{},`user ${like?"like":"unlike"} tweet successsfully`)
+)
+
 }
 )
 
 const getLikedVideos = AsyncHandler(async (req, res) => {
-    //TODO: get all liked videos
-})
+    const userId = req.user._id;
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "This user id is not valid");
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+
+    const likes = await Like.aggregate([
+        {
+            $match: { likedBy: new mongoose.Types.ObjectId(userId) } // filter only current user's likes
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "likedVideos",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "videoOwner",
+                            foreignField: "_id",
+                            as: "videoOwner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            videoOwner: { $arrayElementAt: ["$videoOwner", 0] }
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$likedVideos"
+        },
+        {
+            $replaceRoot: { newRoot: "$likedVideos" } // flatten structure to return videos directly
+        }
+    ]);
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            likes,
+            "Fetched liked videos successfully!!"
+        )
+    );
+});
+
 
 export {
     toggleCommentLike,
