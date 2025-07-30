6eac76dbaa85cbd912401ccb221b99d7974cef17
Shailesh Kumare <shaileshyadav02032003@gmail.com>
2025-07-31 02:28:39 +0530

    done

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -8,8 +8,68 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 
 const getAllVideos = AsyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    
     //TODO: get all videos based on query, sort, pagination
+     const { 
+
+        page = 1,
+        limit = 10,
+        query = `/^video/`,
+        sortBy = "createdAt",
+        sortType = 1, 
+        userId = req.user._id } = req.query
+
+    // find user in db
+    const user = await User.findById(
+        {
+            _id: userId
+        }
+    )
+
+    if(!user){
+        throw new ApiError(404, "user not found")
+    }
+
+    const getAllVideosAggregate = await Video.aggregate([
+        {
+            $match: { 
+                videoOwner: new mongoose.Types.ObjectId(userId),
+                   $or: [
+                    { title: { $regex: query, $options: 'i' } },
+                    { description: { $regex: query, $options: 'i' } }
+                ]
+            }
+        },
+        {
+            $sort:{
+                [sortBy]: sortType
+            }
+        },
+        {
+            $skip: (page -1) * limit
+        },
+        {
+            $limit: parseInt(limit)
+        }
+
+    ])
+
+    Video.aggregatePaginate(getAllVideosAggregate, {page, limit})
+    .then((result)=>{
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                result,
+                "fetched all videos successfully !!"
+            )
+        )
+    })
+    .catch((error)=>{
+        console.log("getting error while fetching all videos:",error)
+        throw error
+    })
 })
 
 const publishAVideo = AsyncHandler(async (req, res) => {
@@ -71,6 +131,28 @@ const publishAVideo = AsyncHandler(async (req, res) => {
 const getVideoById = AsyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+     if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "This video id is not valid")
+    } 
+
+    const video = await Video.findById(
+        {
+            _id: videoId
+        }
+    )
+
+    if(!video){
+        throw new ApiError(404, "video not found")
+    }
+
+    // return responce
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            video,
+            "video fetched successfully!!"
+        )
+    )
 })
 
 const updateVideo = AsyncHandler(async (req, res) => {
@@ -139,10 +221,85 @@ const updateVideo = AsyncHandler(async (req, res) => {
 const deleteVideo = AsyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: delete video
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "This video id is not valid")
+    } 
+
+    // find video in db
+    const video = await Video.findById(
+        {
+            _id: videoId
+        }
+    )
+
+    if(!video){
+        throw new ApiError(404, "video not found")
+    }
+
+    if (video.Owner.toString() !== req.user._id.toString()) {
+         throw new ApiError(403, "You don't have permission to delete this video!");
+    }
+
+    // delete video and thumbnail in cloudinary
+    if(video.videoFile){
+        await deleteOnCloudinary(video.videoFile.public_id, "video")
+    }
+
+    if(video.thumbnail){
+        await deleteOnCloudinary(video.thumbnail.public_id)
+    }
+
+    const deleteResponce = await Video.findByIdAndDelete(videoId)
+
+    if(!deleteResponce){
+        throw new ApiError(500, "something went wrong while deleting video !!")
+    }
+
+    // return responce
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            deleteResponce,
+            "video deleted successfully!!"
+        )
+    )
 })
 
 const togglePublishStatus = AsyncHandler(async (req, res) => {
-    const { videoId } = req.params
+     const { videoId } = req.params
+
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "This video id is not valid")
+    } 
+
+     // find video in db
+     const video = await Video.findById(
+        {
+            _id: videoId
+        }
+    )
+
+    if(!video){
+        throw new ApiError(404, "video not found")
+    }
+
+    if (video.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You don't have permission to toggle this video!")
+    }
+
+    // toggle video status
+    video.isPublished = !video.isPublished
+
+    await video.save({validateBeforeSave: false})
+
+    //return responce 
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            video,
+            "video toggle successfully!!"
+        )
+    )
 })
 
 export {
